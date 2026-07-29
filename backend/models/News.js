0922e8e847b3d294755d0a6dc674b8808b99d409
qb1968 <mark.allison75@gmail.com>
2026-07-29 15:37:35 -0400

    adding news

--- backend/models/News.js
+++ backend/models/News.js
@@ -1,9 +1,27 @@
 import mongoose from "mongoose";
 
-const newsSchema = new mongoose.Schema({
-  title: String,
-  content: String,
-  createdAt: { type: Date, default: Date.now },
-});
+const newsSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    content: {
+      type: String,
+      required: true,
+    },
+
+    date: {
+      type: Date,
+      required: true,
+    },
+
+    image: String,
+  },
+  {
+    timestamps: true,
+  },
+);
 
 export default mongoose.model("News", newsSchema);
