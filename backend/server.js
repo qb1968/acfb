145457cf0712ad692388daacafb9fb2bdeda1ad9
qb1968 <mark.allison75@gmail.com>
2026-07-29 20:08:10 -0400

    officer3

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import authRoutes from "./routes/auth.js";
 import eventRoutes from "./routes/events.js";
 import galleryRoutes from "./routes/gallery.js";
 import newsRoutes from "./routes/news.js";
-import officerRoutes from "./routes/Officer.js";
+import officerRoutes from "./routes/officers.js";
 
 dotenv.config();
 
